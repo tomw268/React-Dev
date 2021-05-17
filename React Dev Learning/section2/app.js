4bbdcf67cc83a41f0dc132aa3978dc5cb7d93057
tomw268 <tomw268@gmail.com>
2021-05-17 13:47:11 -0400

    went back over some js

---
+++ React Dev Learning/section2/app.js
@@ -0,0 +1,28 @@
+// UNDERSTANDING LET AND CONST
+
+//let // can be changed but not need to be
+//const // use for things that will never change like DOB
+
+let myName = 'Tom';
+console.log(myName);
+// logs Tom
+
+myName = 'Manu';
+console.log(myName);
+// logs manu
+
+// UNDERSTANDING ARROW FUNCTIONS
+
+// EXPORTS AND IMPORTS
+
+// UNDERSTANING CLASSES
+
+// CLASS, PROPERTIES AND METHODS
+
+// THE SPREAD AND REST OPERATOR
+
+// DESTRUCTURING
+
+// REF AND PRIM TIMES REFRESHER
+
+// REFESH ARRAY FUNCTIONS
