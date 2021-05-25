ef03e77fce6b69ddb767413144d79c01e835712b
tomw268 <tomw268@gmail.com>
2021-05-25 13:44:25 -0400

    made some update to my component

--- expense-app/src/components/ExpenseItem.js
+++ expense-app/src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-  return <h2>Expense Item!</h2>;
+  return (
+    <div>
+      <div>May 25th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$143.62</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
