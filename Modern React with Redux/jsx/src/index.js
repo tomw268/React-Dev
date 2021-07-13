5800796b4124ceeb8b5577b2e37ad971787bb6c7
tomw268 <tomw268@gmail.com>
2021-07-13 13:21:58 -0400

    fir component is built

--- Modern React with Redux/jsx/src/index.js
+++ Modern React with Redux/jsx/src/index.js
@@ -2,5 +2,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 // Create a React component
-
+const App = () => {
+  return <div>Hi There!</div>;
+};
 // Take the react component and show on screen
+ReactDom.render(<App />, document.querySelector('#root'));
