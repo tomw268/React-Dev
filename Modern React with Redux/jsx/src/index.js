3148571e0180625049bcf6a78ac4cb1e4dc255cb
tomw268 <tomw268@gmail.com>
2021-07-14 09:07:04 -0400

    working with some bugs

--- Modern React with Redux/jsx/src/index.js
+++ Modern React with Redux/jsx/src/index.js
@@ -3,7 +3,15 @@ import React from 'react';
 import ReactDom from 'react-dom';
 // Create a React component
 const App = () => {
-  return <div>Hi There!</div>;
+  return (
+    <div>
+      <label for="name" class="label">
+        Enter Name:
+      </label>
+      <input id="name" type="text" />
+      <button style={'background-color: blue; color:white'}>Submit</button>
+    </div>
+  );
 };
 // Take the react component and show on screen
 ReactDom.render(<App />, document.querySelector('#root'));
