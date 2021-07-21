4c2d0ce81ba18aef544fbac1047037a8d75d0e53
tomw268 <tomw268@gmail.com>
2021-07-21 14:27:33 -0400

    added images

--- Modern React with Redux/components/src/index.js
+++ Modern React with Redux/components/src/index.js
@@ -7,7 +7,7 @@ const App = () => {
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" src={faker} />
+          <img alt="avatar" src={faker.img.avatar()} />
         </a>
         <div className="content">
           <a href="/" className="author">
