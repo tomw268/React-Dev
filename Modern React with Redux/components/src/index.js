f2eec6de412ad44ac3da50c251ef873df5fcf1c1
tomw268 <tomw268@gmail.com>
2021-07-22 13:28:06 -0400

    made more post comments

--- Modern React with Redux/components/src/index.js
+++ Modern React with Redux/components/src/index.js
@@ -19,6 +19,34 @@ const App = () => {
           <div className="text">Nice Blog Post!!</div>
         </div>
       </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Tom
+          </a>
+          <div className="metadata">
+            <span className="date">Today @ 6:00 pm</span>
+          </div>
+          <div className="text">Nice Blog Post!!</div>
+        </div>
+      </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Matt
+          </a>
+          <div className="metadata">
+            <span className="date">Today @ 6:00 pm</span>
+          </div>
+          <div className="text">Nice Blog Post!!</div>
+        </div>
+      </div>
     </div>
   );
 };
