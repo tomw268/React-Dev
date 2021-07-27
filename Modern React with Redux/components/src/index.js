6c20cf0c0f1d6f03835a219d8471b6fd7d1eb8df
tomw268 <tomw268@gmail.com>
2021-07-27 13:41:50 -0400

    udated files

--- Modern React with Redux/components/src/index.js
+++ Modern React with Redux/components/src/index.js
@@ -11,20 +11,6 @@ const App = () => {
         </a>
         <div className="content">
           <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today @ 6:00 pm</span>
-          </div>
-          <div className="text">Nice Blog Post!!</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
             Tom
           </a>
           <div className="metadata">
