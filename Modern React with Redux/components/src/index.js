82870f58bd7351a996e05c3d6c36c812c857c6bf
tomw268 <tomw268@gmail.com>
2021-08-12 13:29:55 -0400

    delted card coded comments

--- Modern React with Redux/components/src/index.js
+++ Modern React with Redux/components/src/index.js
@@ -4,38 +4,7 @@ import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
-  return (
-    <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Matt
-          </a>
-          <div className="metadata">
-            <span className="date">Today @ 6:00 pm</span>
-          </div>
-          <div className="text">Nice Blog Post!!</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Matt
-          </a>
-          <div className="metadata">
-            <span className="date">Today @ 6:00 pm</span>
-          </div>
-          <div className="text">Nice Blog Post!!</div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="ui container comments"></div>;
 };
 
 ReactDom.render(<App />, document.querySelector('#root'));
