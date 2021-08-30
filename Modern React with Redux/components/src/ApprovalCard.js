f58c6b5b67200ca49451da21ac84f251fb6f7c1e
tomw268 <tomw268@gmail.com>
2021-08-30 13:31:41 -0400

    added some ui to the new component

--- Modern React with Redux/components/src/ApprovalCard.js
+++ Modern React with Redux/components/src/ApprovalCard.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from 'react';
 
-const ApprovalCard = () +>{
+const ApprovalCard = () => {
+  return (
+    <div className="ui card">
+      <div className="content">Are you sure?</div>
+      <div className="extra content"></div>
+    </div>
+  );
+};
 
-}
-
-export default ApprovalCard
+export default ApprovalCard;
