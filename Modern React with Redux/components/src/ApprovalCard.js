2dad51968d70415aeb9756032d1dc8cdde6aade8
tomw268 <tomw268@gmail.com>
2021-08-30 13:39:39 -0400

    fixed the button bug

--- Modern React with Redux/components/src/ApprovalCard.js
+++ Modern React with Redux/components/src/ApprovalCard.js
@@ -4,7 +4,12 @@ const ApprovalCard = () => {
   return (
     <div className="ui card">
       <div className="content">Are you sure?</div>
-      <div className="extra content"></div>
+      <div className="extra content">
+        <div className="ui two buttons">
+          <div className="ui  green button">Approve</div>
+          <div className="ui red button">Decline</div>
+        </div>
+      </div>
     </div>
   );
 };
