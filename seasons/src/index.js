27fbbf43bdfddc17afd606a428ec2f6fca2199bf
tomw268 <tomw268@gmail.com>
2021-09-09 12:08:32 -0400

    adding in a contructor function

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  // REact says we have to define Render!!!!
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
